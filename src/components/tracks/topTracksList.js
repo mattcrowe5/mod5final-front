308685001365ee3d18632d8d19b324b323f378a7
mattcrowe5 <crowe.matthew5@gmail.com>
2017-12-19 14:07:12 -0500

    updated top track rendering

--- src/components/tracks/topTracksList.js
+++ src/components/tracks/topTracksList.js
@@ -1,56 +1,24 @@
-import _ from "lodash";
 import React, { Component } from "react";
-import { Button, Image, List, Transition } from "semantic-ui-react";
+import { Card, Image } from "semantic-ui-react";
 
-export default class TopTracksList extends Component {
-  state = { tracks: this.props.toptracks.slice(0, 3) };
-
-  handleAdd = () =>
-    this.setState({
-      tracks: this.props.toptracks.slice(0, this.state.tracks.length + 1)
-    });
-
-  handleRemove = () =>
-    this.setState({ tracks: this.state.tracks.slice(0, -1) });
-
+class TopTracksList extends Component {
   render() {
-    const { tracks } = this.state;
-    const division = " - ";
-
     return (
-      <div>
-        <Button.Group>
-          <Button
-            disabled={tracks.length === 0}
-            icon="minus"
-            onClick={this.handleRemove}
-          />
-          <Button
-            disabled={tracks.length === this.props.toptracks.length}
-            icon="plus"
-            onClick={this.handleAdd}
-          />
-        </Button.Group>
-
-        <Transition.Group
-          as={List}
-          duration={200}
-          divided
-          size="huge"
-          verticalAlign="middle"
-        >
-          {tracks.map(track => (
-            <List.Item key={track.name}>
-              <Image avatar src={track.album.images[1].url} />
-              <List.Content
-                header={_.startCase(
-                  `${track.name} ${division} ${track.artists[0].name}`
-                )}
-              />
-            </List.Item>
-          ))}
-        </Transition.Group>
-      </div>
+      <Card.Group itemsPerRow={6}>
+        {this.props.toptracks.map(track => (
+          <Card>
+            <Image src={track.album.images[0].url} />
+            <Card.Content>
+              <Card.Header>{track.name}</Card.Header>
+              <Card.Meta>
+                <span>{track.artists[0].name}</span>
+              </Card.Meta>
+            </Card.Content>
+          </Card>
+        ))}
+      </Card.Group>
     );
   }
 }
+
+export default TopTracksList;
